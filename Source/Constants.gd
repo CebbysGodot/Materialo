3e0a005f36c79c2ea01058d37d215379b4a4b2bc
CebbyS <cebbys.dev@gmail.com>
2020-05-14 01:50:16 +0300

    File containing all neccecary values for Materialo

---
+++ Source/Constants.gd
@@ -0,0 +1,15 @@
+extends Resource
+class_name MaterialoConstants
+
+const CHUNK_SIZE = 16
+const BLOCK_TEXTURE_SIZE = 32
+
+# Resource paths
+const PATH_TEXTURE_FILE_RESOURCE = "res://Game/Resources/Textures.tres"
+const PATH_MOD_DIRECTORY = "res://Game/Mods/"
+
+const PATH_ENTITY_LIST_RESOURCE = "res://Game/Resources/EntityList.tres"
+const PATH_ENTITY_LIST = "res://Game/Resources/"
+
+# Missing texture name
+const MISSING_TEXTURE_NAME = "missing_texture"
