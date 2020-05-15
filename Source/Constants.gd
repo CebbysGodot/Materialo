70d9390a9ece82b8cc7281bf48693a437163d702
CebbyS <cebbys.dev@gmail.com>
2020-05-15 12:39:17 +0300

    Refactored constants and removed unused ones

--- Source/Constants.gd
+++ Source/Constants.gd
@@ -8,8 +8,7 @@ const BLOCK_TEXTURE_SIZE = 32
 const PATH_TEXTURE_FILE_RESOURCE = "res://Game/Resources/Textures.tres"
 const PATH_MOD_DIRECTORY = "res://Game/Mods/"
 
+const PATH_RESOURCES = "res://Game/Resources/"
 const PATH_ENTITY_LIST_RESOURCE = "res://Game/Resources/EntityList.tres"
-const PATH_ENTITY_LIST = "res://Game/Resources/"
-
-# Missing texture name
-const MISSING_TEXTURE_NAME = "missing_texture"
+const PATH_TEXTURE_LIST_RESOURCE = "res://Game/Resources/TextureList.tres"
+const PATH_MISSING_TEXTURE = "res://Resource/missing_texture.png"
