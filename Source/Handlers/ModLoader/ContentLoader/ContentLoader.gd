754438462d56bc35b2debd4dbadf1b09544b65ba
CebbyS <cebbys.dev@gmail.com>
2020-05-15 12:38:02 +0300

    Updated Content loader with texture loading

--- Source/Handlers/ModLoader/ContentLoader/ContentLoader.gd
+++ Source/Handlers/ModLoader/ContentLoader/ContentLoader.gd
@@ -1,28 +1,36 @@
 tool
 class_name MaterialoContentLoader
 
-var BLOCK_LOADER
+var BLOCK_LOADER:MaterialoBlockLoader
+var TEXTURE_LOADER:MaterialoTextureLoader
 var MATERIALO_ENTITY_LIST:MaterialoEntityListTemplate
 
 func _init():
 	BLOCK_LOADER = MaterialoBlockLoader.new()
+	TEXTURE_LOADER = MaterialoTextureLoader.new()
 
 
 func generate_content(mod_structures:Array):
-	_load_entity_list()
+	load_entity_list()
 	for mod in mod_structures:
-		_load_content(mod)
+		load_content(mod)
+		load_content_textures(mod)
 
 
 
-func _load_content(mod:MaterialoModStructure):
+func load_content(mod:MaterialoModStructure):
 	MATERIALO_ENTITY_LIST = BLOCK_LOADER.load_block_list(MATERIALO_ENTITY_LIST, mod)
-	_save_entity_list()
+	save_entity_list()
 
-func _load_entity_list():
+func load_content_textures(mod:MaterialoModStructure):
+	TEXTURE_LOADER.initialise()
+	TEXTURE_LOADER.load_block_textures(mod)
+	TEXTURE_LOADER.save_resource()
+
+func load_entity_list():
 	var directory_parser = Directory.new()
-	if not directory_parser.dir_exists(MaterialoConstants.PATH_ENTITY_LIST):
-		directory_parser.make_dir_recursive(MaterialoConstants.PATH_ENTITY_LIST)
+	if not directory_parser.dir_exists(MaterialoConstants.PATH_RESOURCES):
+		directory_parser.make_dir_recursive(MaterialoConstants.PATH_RESOURCES)
 	if not directory_parser.file_exists(MaterialoConstants.PATH_ENTITY_LIST_RESOURCE):
 		if ResourceSaver.save(
 			MaterialoConstants.PATH_ENTITY_LIST_RESOURCE,
@@ -30,7 +38,7 @@ func _load_entity_list():
 		) == FAILED: print("Saving entity list failed")
 	MATERIALO_ENTITY_LIST = load(MaterialoConstants.PATH_ENTITY_LIST_RESOURCE)
 
-func _save_entity_list():
+func save_entity_list():
 	if ResourceSaver.save(
 		MaterialoConstants.PATH_ENTITY_LIST_RESOURCE,
 		MATERIALO_ENTITY_LIST
