b11e01e2431d48c9a9826f8b24a338426652d840
CebbyS <cebbys.dev@gmail.com>
2020-05-15 12:38:43 +0300

    Finished basic block loading component

--- Source/Handlers/ModLoader/ContentLoader/BlockLoader/BlockLoader.gd
+++ Source/Handlers/ModLoader/ContentLoader/BlockLoader/BlockLoader.gd
@@ -4,6 +4,9 @@ class_name MaterialoBlockLoader
 func create_block( block_json:Dictionary ):
 	var block = BaseBlock.new()
 	block.block_name = block_json[ "name" ]
+	var model = BaseBlockModel.new()
+	model.model_textures = block_json[ "textures" ]
+	block.block_model = model
 
 	return block
 
@@ -21,12 +24,22 @@ func load_block_list( materialo_entities:MaterialoEntityListTemplate, structure:
 				if not directory_parser.current_is_dir():
 					var json_path = "%s%s" % [ path, block_json ]
 					file_parser.open( json_path, File.READ )
-					var block = create_block( parse_json( file_parser.get_as_text() ) )
-					var block_id = structure.get_block_id( block.block_name )
-					materialo_entities.BLOCK_LIST[ block_id ] = block
+					var json = parse_json( file_parser.get_as_text() )
+					file_parser.close()
+					if verify_block_json( json ):
+						var block = create_block( json )
+						var block_id = structure.get_block_id( block.block_name )
+						materialo_entities.BLOCK_LIST[ block_id ] = block
+					else:
+						print("[%s] json file is invalid" % [json_path])
 
 				block_json = directory_parser.get_next()
 			directory_parser.list_dir_end()
 		else: print( "Opening block dir[%s] failed!" % [ path ] )
 
 	return materialo_entities
+
+func verify_block_json( block_json:Dictionary ) -> bool:
+	if not block_json.has( "name" ): return false
+	if not block_json.has( "textures" ): return false
+	return true
