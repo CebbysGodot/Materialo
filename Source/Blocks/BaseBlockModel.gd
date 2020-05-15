8de5734f5ee89de14ddca6f8e150271822fd0703
CebbyS <cebbys.dev@gmail.com>
2020-05-15 12:38:57 +0300

    Finished basic block loading component

--- Source/Blocks/BaseBlockModel.gd
+++ Source/Blocks/BaseBlockModel.gd
@@ -1,8 +1,8 @@
 tool extends Resource
 class_name BaseBlockModel
 
-export(Array) var _model_textures
-export(Array) var _model_faces
+export(Dictionary) var model_textures
+export(Array) var model_faces
 
 func get_class() -> String:
     return "BaseBlockModel"
